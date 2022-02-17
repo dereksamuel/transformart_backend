93148a09fa6607affcf7c29067f538fc0c15b4d1
Derek Paul <dereksamuelgr@gmail.com>
2022-02-17 14:21:44 -0500

    added cors

--- src/index.js
+++ src/index.js
@@ -5,12 +5,28 @@ require("dotenv").config({
 });
 
 const express = require("express");
+const cors = require("cors");
 const { routerApi } = require("./router/");
 
 // intializations
 const app = express();
+const whiteList = [
+	"http://localhost:4001",
+	"http://localhost:4000"
+];
+
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (whiteList.find((whiteItem) => whiteItem === origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+};
 
 // middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT;
