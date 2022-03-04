84fab4ff24825d3823f0b6e1260375fe0e0b3337
Derek Paul <dereksamuelgr@gmail.com>
2022-03-04 13:40:34 -0500

    test to the correct way

--- src/tests/products.test.js
+++ src/tests/products.test.js
@@ -4,7 +4,6 @@ const productQueries = require("../resolvers/queries/product.queries");
 
 const { JSONparser } = require("../utils/parser");
 
-const MODELS = process.models;
 const productMock = {
   name: "Tester product",
   price: 450000,
@@ -21,28 +20,58 @@ describe("-- Product Queries --", () => {
   let mutations = null;
   let queries = null;
 
+  let products = [];
+  let product = null;
+  let productCreated = null;
+
   beforeAll(() => {
-    queries = productQueries(MODELS);
-    mutations = productMutations(MODELS);
+    queries = productQueries({ models: process.models });
+    mutations = productMutations({ models: process.models });
   });
 
   test("getProducts", async () => {
-    const products = JSONparser(await queries.getProducts());
+    products = JSONparser(await queries.getProducts());
 
     expect(products).not.toBe(undefined);
     expect(products.length).not.toBe(0);
   });
 
-  test("createProduct -- When I am not authorized", async () => {
-    const product = JSONparser(await mutations.createProduct(null, productMock));
-    // FIXME: Fix me later, with complete authorized protocol with firebase
+  test("getProduct", async () => {
+    product = JSONparser(await queries.getProduct(null, { productId: `${products[0].id}` }));
 
-    expect(product).toBeFalsy();
+    expect(product).not.toBe(undefined);
+    expect(product.length).not.toBe(0);
   });
 
-  test("createProduct -- When I am authorized", async () => {
-    const product = JSONparser(await mutations.createProduct(null, productMock));
+  // test("createProduct -- When I am not authorized", async () => {
+  //   const product = JSONparser(await mutations.createProduct(null, productMock));
+  //   // FIXME: Fix me later, with complete authorized protocol with firebase
 
-    expect(product).toBeTruthy();
+  //   expect(product).toBeFalsy();
+  // });
+
+  test("createProduct -- When I am authorized", async () => {
+    productCreated = JSONparser(await mutations.createProduct(null, productMock));
+
+    expect(productCreated).toBeTruthy();
+  });
+
+  test("updateProduct -- When I am authorized", async () => {
+    productCreated = JSONparser(await mutations.updateProduct({
+      Query: queries
+    }, {
+      id: productCreated.id,
+      ...productMock
+    }));
+
+    expect(productCreated).toBeTruthy();
+  });
+
+  test("deleteProduct -- When I am authorized", async () => {
+    const productDeleted = JSONparser(await mutations.deleteProduct({
+      Query: queries
+    }, { id: productCreated.id }));
+
+    expect(productDeleted).toBeTruthy();
   });
 });
