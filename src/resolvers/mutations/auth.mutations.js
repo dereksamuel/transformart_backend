eed173150406b77239ad44726489ea3d0b8471b8
Derek Paul <11dereksamuel@gmail.com>
2022-04-01 07:25:18 -0500

    logout mutation completed 🚪

--- src/resolvers/mutations/auth.mutations.js
+++ src/resolvers/mutations/auth.mutations.js
@@ -1,5 +1,10 @@
+const { firebaseAdmin } = require("../../utils/firebase");
+
 module.exports = () => ({
   async logout() {
+    const uid = "eOtXEmtY2FWA2mjsdxgYdRG02jl2";
+
+    await firebaseAdmin.auth().revokeRefreshTokens(uid);
     global.decodedToken = null;
   },
 
