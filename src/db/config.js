a15f6d2076e04f6816b36a541c92a95cf26f289f
Derek Paul <dereksamuelgr@gmail.com>
2023-06-05 13:31:25 -0500

    done

--- src/db/config.js
+++ src/db/config.js
@@ -2,8 +2,8 @@ const { development_URI, production_URI } = require("../utils/keys");
 
 module.exports = {
   development: {
-    url: development_URI,
-    dialect: "sqlite",
+    url: production_URI,
+    dialect: "mysql",
   },
   production: {
     url: production_URI,
